1c4eb14a97ac0b98029131ab59858029b95d8fb6
Juan Clot <clot.juan@gmail.com>
2018-08-17 01:57:56 -0300

    asd

---
+++ sorter/algos.go
@@ -0,0 +1,72 @@
+package sorter
+
+import (
+	"image/color"
+	"math"
+	"math/rand"
+)
+
+func dis(m img, n *img, i, j int) {
+	if i > 15 && j > 13 {
+		ni, nj := i-15, j-13
+		n.m[i][j].R, n.m[i][j].G, n.m[i][j].B, n.m[i][j].A = m.m[ni][nj].R, m.m[ni][nj].G, m.m[ni][nj].B, m.m[ni][nj].A
+
+	}
+}
+func datdaat(m img, n *img, i, j int) {
+	if dat(m, n, i, j) && daat(m, n, i, j) {
+		n.m[i][j] = m.m[i][j]
+	}
+}
+
+func dat(m img, n *img, i, j int) bool {
+	if rgbAvg(m.m[i][j]) {
+		nR := m.m[i][j].R
+		nG := m.m[i][j].G + 60
+		nB := m.m[i][j].B - 30
+		n.m[i][j].R, n.m[i][j].G, n.m[i][j].B, n.m[i][j].A = nR, nG, nB, m.m[i][j].A
+		return false
+	} else {
+		//n.m[i][j] = m.m[i][j]
+		return true
+	}
+}
+
+func rgbAvg(c color.RGBA) bool {
+	avg := int((c.R + c.G + c.B + c.A) / 4)
+	dr, dg, db := abs(uint8(avg)-c.R), abs(uint8(avg)-c.G), abs(uint8(avg)-c.B)
+	return dr < 120 && dg < 130 && db < 90
+}
+
+func daat(m img, n *img, i, j int) bool {
+	ni, nj := i+random(-52, 52), j+random(-52, 52)
+	//fmt.Println(ni, nj)
+	canI := ni < len(m.m) && ni > 100
+	canJ := nj < len(m.m[i]) && nj > 100
+	//fmt.Println(canI, canJ)
+	if canI && canJ {
+		n.m[i][j] = m.m[ni][nj]
+		return false
+	} else {
+		//n.m[i][j] = m.m[i][j]
+		return true
+	}
+}
+
+func juat(m img, n *img, i, j int, pch *chan color.RGBA) {
+	datdaat(m, n, i, j)
+	c := m.m[i][j]
+	if int((c.R+c.G+c.B)/3) > 150 {
+		*pch <- n.m[i][j]
+	}
+}
+
+// _____________________________common_____________________________
+
+func abs(n uint8) uint8 {
+	return uint8(math.Abs(float64(n)))
+}
+
+func random(min, max int) int {
+	return rand.Intn(max-min) + min
+}
--- sorter/sorter.go
+++ sorter/sorter.go
@@ -5,9 +5,10 @@ import (
 	"image"
 	"image/color"
 	"io"
-	"math"
+	"math/rand"
 	"os"
 	"sync"
+	"time"
 )
 
 // Pixel struct example
@@ -16,12 +17,15 @@ type img struct {
 	h, w int
 	m    [][]color.RGBA
 }
+type Px color.RGBA
 
 func (m *img) At(x, y int) color.Color { return m.m[x][y] }
 func (m *img) ColorModel() color.Model { return color.RGBAModel }
 func (m *img) Bounds() image.Rectangle { return image.Rect(0, 0, m.h, m.w) }
 
+// Sort wraps the sorting process
 func Sort(imgfile io.ReadCloser) image.Image {
+	rand.Seed(time.Now().Unix())
 	pixels, h, w, err := GetPixels(imgfile)
 	if err != nil {
 		fmt.Println("Error: Image could not be decoded")
@@ -48,7 +52,7 @@ func GetPixels(imgfile io.ReadCloser) ([][]color.RGBA, int, int, error) {
 
 	bounds := img.Bounds()
 	width, height := bounds.Max.X, bounds.Max.Y
-	fmt.Println(height, width)
+
 	var pixels [][]color.RGBA
 	for x := 0; x < width; x++ {
 		var row []color.RGBA
@@ -57,7 +61,7 @@ func GetPixels(imgfile io.ReadCloser) ([][]color.RGBA, int, int, error) {
 		}
 		pixels = append(pixels, row)
 	}
-
+	fmt.Println(height, width)
 	return pixels, height, width, nil
 }
 
@@ -71,19 +75,25 @@ func rgbaToRGBA(r uint32, g uint32, b uint32, a uint32) color.RGBA {
 // user	0m40.615s
 // sys	0m2.517s
 func workersFillImg(m img, n *img) {
+
 	workers := 32
 	var wg sync.WaitGroup
 	wg.Add(workers)
 	c := make(chan struct{ i, j int })
+	pch := make(chan color.RGBA, 100)
 	for i := 0; i < workers; i++ {
 		go func() {
 			for t := range c {
-				fillPixel(m, n, t.i, t.j)
+				fillPixel(m, n, t.i, t.j, &pch)
 			}
 			wg.Done()
 		}()
 	}
-
+	go func() {
+		for m := range pch {
+			fmt.Println("esteee ", m)
+		}
+	}()
 	for i, row := range m.m {
 		for j := range row {
 			c <- struct{ i, j int }{i, j}
@@ -93,35 +103,9 @@ func workersFillImg(m img, n *img) {
 	wg.Wait()
 }
 
-func fillPixel(m img, n *img, i, j int) {
+func fillPixel(m img, n *img, i, j int, pch *chan color.RGBA) {
 	//dis(m, n, i, j)
-	dat(m, n, i, j)
+	//datdaat(m, n, i, j)
+	juat(m, n, i, j, pch)
 
-}
-func dis(m img, n *img, i, j int) {
-	if i > 15 && j > 13 {
-		ni, nj := i-15, j-13
-		n.m[i][j].R, n.m[i][j].G, n.m[i][j].B, n.m[i][j].A = m.m[ni][nj].R, m.m[ni][nj].G, m.m[ni][nj].B, m.m[ni][nj].A
-
-	}
-}
-func dat(m img, n *img, i, j int) {
-	if rgbAvg(m.m[i][j]) {
-		nR := m.m[i][j].R - 20
-		nG := m.m[i][j].G + 90
-		nB := m.m[i][j].B - 30
-		n.m[i][j].R, n.m[i][j].G, n.m[i][j].B, n.m[i][j].A = nR, nG, nB, m.m[i][j].A
-	} else {
-		n.m[i][j] = m.m[i][j]
-	}
-}
-
-func rgbAvg(c color.RGBA) bool {
-	avg := int((c.R + c.G + c.B + c.A) / 4)
-	dr, dg, db := abs(uint8(avg)-c.R), abs(uint8(avg)-c.G), abs(uint8(avg)-c.B)
-	return dr < 120 && dg < 130 && db < 90
-}
-
-func abs(n uint8) uint8 {
-	return uint8(math.Abs(float64(n)))
 }
